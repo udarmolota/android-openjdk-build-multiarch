1bbcf0b4cd0105266073448c914a95fdd54b4847
Inna Z <45328865+udarmolota@users.noreply.github.com>
2026-01-29 20:27:40 +0200

    Add jdk25

--- 5_clonejdk.sh
+++ 5_clonejdk.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
 
-if [[ $TARGET_VERSION -eq 21 ]]; then
+case "$TARGET_VERSION" in
+  25)
+    # JDK 25 updates repo
+    git clone --depth 1 https://github.com/openjdk/jdk25u openjdk-25
+    ;;
+  21)
     git clone --branch jdk21.0.1 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
-else
+    ;;
+  17|*)
     git clone --depth 1 https://github.com/openjdk/jdk17u openjdk-17
-fi
+    ;;
+esac
