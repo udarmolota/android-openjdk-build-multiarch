b1dda678e035d0b06ac6c2d14ae5197973ca5082
Inna Z <45328865+udarmolota@users.noreply.github.com>
2026-01-29 20:44:15 +0200

    Add jdk-25.0.1+9 tag to jdk25

--- 5_clonejdk.sh
+++ 5_clonejdk.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [[ $TARGET_VERSION -eq 25 ]]; then
-    git clone --branch <tag> --depth 1 https://github.com/openjdk/jdk25u openjdk-25
+    git clone --branch jdk-25.0.1+9 --depth 1 https://github.com/openjdk/jdk25u openjdk-25
 elif [[ $TARGET_VERSION -eq 21 ]]; then
     git clone --branch jdk21.0.1 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
 else
